4b3449dc5b60f0b709a8aa64c96ac17470bf6790
Aditi Kumar <aditi.kr158@gmail.com>
2018-01-03 17:56:48 +0530

    Update pollutantsAnalysisMG.R

--- scripts/MandirMarg/pollutantsAnalysisMG.R
+++ scripts/MandirMarg/pollutantsAnalysisMG.R
@@ -1,15 +1,13 @@
 #set the working directory
-
-path<-"~/Desktop/IT NSIT/6th sem/Project/Data Science/Mandir Marg/Pollutants/Data Sets"
+path<-"~/Desktop/Data Science/Mandir Marg/Pollutants/Data Sets"
 setwd(path)
 
-#PM10
+#PM10 Mandir Marg
 #Load Datasets
 pm10mg <- read.csv("pm10MandirMarg.csv")
 attach(pm10mg)
 
-
-#Initiate the libraries 
+#Import the libraries 
 library("lubridate")
 library("plyr")
 library("ggplot2")
@@ -43,7 +41,6 @@ if(any(is.na(pm10mg))){
     else{pm10mg[which(is.na(pm10mg$Unit)),][6][i,]<-0}}
 }
 
-
 #date manipulation, for changing the date format
 pm10mg$Date<- as.Date(pm10mg$Date,format = "%d/%m/%Y")
 month(as.POSIXlt(pm10mg$Date, format="%Y/%m/%d"))
@@ -54,11 +51,9 @@ levels(Months)
 # Add a new Variable called "Months"
 pm10mg <- cbind(pm10mg,Months)
 
-
 #We can see the data of a paricular month 
 pm10mg[Months=="Oct",]
 pm10mg[Months=="Oct" & Concentration<400,] # do some analysis ,add one more column of levels : low, moderate , high ,according to Concentration
-
 
 #Adding a Column to show Exceedence Factor
 ExceedenceFactor<- pm10mg$Concentration/std
@@ -69,23 +64,13 @@ attach(pm10mg)
 #Now,Lets see the summary 
 summary(pm10mg)
 
-
 #plotting
-#WHO Guideline = 50
 par(bg="bisque")
 
-#barplot(table(PollutionType),main = "Pollution Category",xlab ="Categories",ylab = "No. Of Days",las=1, names.arg = c(levels(PollutionType)),ylim = c(0,100),col ="damgorchid4",cex.axis = 0.8,cex.names =0.8)
-#box()
 table2<-table(PollutionType,Months)
 barplot(table2,beside = T,main="Mandir Marg: Pollution Category For each Month for PM10",ylab="No.of Days",col=c("sienna1","salmon","orangered1","red3"))
 legend(5,30,legend=c("low","moderate","high","critical"),fill=c("sienna1","salmon","orangered1","red3"),bty="n",cex = 0.6)
 box()
-
-#par(adj=0.5)
-#pie(table(PollutionType),main = "Pollution Category")
-
-#boxplot(Concentration,main="Concentration of PM10",ylim=c(0,1000),las=1)
-#boxplot(Concentration~Months,main="Concentration of PM10 : mg Puram",ylim=c(0,1000),las=1,col=c("coral3","aliceblue","brown1"))
 
 par(mar=c(7,5,3,3))
 boxplot(Concentration~Months*PollutionType,main="Mandir Marg: Concentration of PM10",ylab="Concentration (µg/m3)",ylim=c(0,1000),las=2,col=c("lightpink1","turquoise1","thistle"),cex.axis = 0.8)
@@ -93,18 +78,19 @@ boxplot(Concentration~Months*PollutionType,main="Mandir Marg: Concentration of P
 pm10mg[Months=="Oct",]$Concentration
 summary(pm10mg[Months=="Oct",]$Concentration)
 
-
-
+#Line Chart
 ggplot(pm10mg,aes(pm10mg$Date,pm10mg$Concentration,color=2))+ geom_line(size = 1,show.legend = FALSE) + xlab("Months") + ylab("Concentration(µg/m3)") + ggtitle(" Mandir Marg : PM10 Concentration vs Months ") + theme(axis.text.x = element_text(angle = 0, vjust = 0.5, color = "navy"))+geom_hline(yintercept = 100,color="red")+geom_hline(yintercept =mean(pm10mg$Concentration),color="orchid3")+ ylim(0, 1000)+geom_text(aes(as.Date("2016-11-15"),124),cex=4,color="red",fontface="italic",label="Prescribed Standard=100")+geom_text(aes(as.Date("2016-10-15"),325),cex=4,color="orchid4",fontface="italic",label="Mean=307.34")
-
 
 detach(pm10mg)
 #rm(list = ls())
 
+#******************************************************************************************************************************
 #NO2 Mandir Marg
 
+#Load File
 no2mg <- read.csv("no2MandirMarg.csv")
 
+#Data Exploration
 dim(no2mg)
 str(no2mg)
 table(is.na(no2mg))
@@ -114,7 +100,6 @@ levels(no2mg$Exceeding.Standard...Yes.No.)
 levels(no2mg$Exceeding.Standard...Yes.No.)[1] <- 0
 levels(no2mg$Exceeding.Standard...Yes.No.)[2] <- 1
 
-
 #date manipulation
 no2mg$Date<- as.Date(no2mg$Date,format = "%d/%m/%Y")
 month(as.POSIXlt(no2mg$Date, format="%Y/%m/%d"))
@@ -122,16 +107,14 @@ Months<- as.factor(month(as.POSIXlt(no2mg$Date, formast="%Y/%m/%d")))
 Months<-mapvalues(Months, from = c(10,11,12), to = c("Oct","Nov","Dec"))
 levels(Months)
 
-
-
 # Add a new Variable called "Months"
 no2mg <- cbind(no2mg,Months)
 
 attach(no2mg)
+
 #We can see the data of a paricular month 
 no2mg[Months=="Oct",]
 no2mg[Months=="Oct" & Concentration<80,] # do some analysis ,add one more column of levels : low, moderate , high ,according to Concentration
-
 
 #Adding a Column to show Exceedence Factor
 ExceedenceFactor<- no2mg$Concentration/std
@@ -139,27 +122,18 @@ no2mg<- cbind(no2mg,ExceedenceFactor)
 PollutionType<-cut(ExceedenceFactor,breaks=c(0,0.5,1.0,1.5,10),labels=c("Low","Moderate","High","Critical"),right = F)
 no2mg<- cbind(no2mg,PollutionType)
 attach(no2mg)
+
 #Now,Lets see the summary 
 summary(no2mg)
 
-
 #plotting
-#WHO Guideline = 50
 par(bg="gray91")
 
-#barplot(table(PollutionType),main = "Pollution Category",xlab ="Categories",ylab = "No. Of Days",las=1, names.arg = c(levels(PollutionType)),ylim = c(0,100),col ="damgorchid4",cex.axis = 0.8,cex.names =0.8)
-#box()
 table2<-table(PollutionType,Months)
 par(mar=c(3,5,3,3))
 barplot(table2,beside = T,main="Mandir Marg: Pollution Category For each Month for NO2",ylab="No.of Days",col=c("sienna1","salmon","orangered1","red3"),ylim =c(0,30))
 legend(5,29,legend=c("low","moderate","high","critical"),fill=c("sienna1","salmon","orangered1","red3"),bty="n",cex =0.7)
 box()
-
-#par(adj=0.5)
-#pie(table(PollutionType),main = "Pollution Category")
-
-#boxplot(Concentration,main="Concentration of PM10",ylim=c(0,1000),las=1)
-#boxplot(Concentration~Months,main="Concentration of PM10 : mg Puram",ylim=c(0,1000),las=1,col=c("coral3","aliceblue","brown1"))
 
 par(mar=c(5,4,1,0))
 boxplot(Concentration~Months*PollutionType,main="Mandir Marg: Concentration of NO2",ylab="Concentration (µg/m3)",ylim=c(20,140),las=2,col=c("lightpink1","turquoise1","thistle"),cex.axis = 0.8)
@@ -167,17 +141,18 @@ boxplot(Concentration~Months*PollutionType,main="Mandir Marg: Concentration of N
 no2mg[Months=="Oct",]$Concentration
 summary(no2mg[Months=="Oct",]$Concentration)
 
-
-
+#Line Charts
 ggplot(no2mg,aes(no2mg$Date,no2mg$Concentration,color=2))+ geom_line(size = 1,show.legend = FALSE) + xlab("Months") + ylab("Concentration(µg/m3)") + ggtitle(" Mandir Marg : NO2 Concentration vs Months ") + theme(axis.text.x = element_text(angle = 0, vjust = 0.5, color = "navy"))+geom_hline(yintercept = 80,color="red")+ ylim(0, 140)+geom_text(aes(as.Date("2016-10-13"),83),cex=3,color=2,fontface="italic",label="Prescribed Standard=80")+geom_hline(yintercept =mean(no2mg$Concentration),color="orchid3")+geom_text(aes(as.Date("2016-10-15"),71),cex=4,color="orchid4",fontface="italic",label="Mean=68.50")
 
 detach(no2mg)
 #rm(list = ls())
 
-#SO2
+#SO2 Mandir Marg
 
+#Load File
 so2mg <- read.csv("so2MandirMarg.csv")
 
+#Data Exploration
 dim(so2mg)
 str(so2mg)
 table(is.na(so2mg))
@@ -202,10 +177,10 @@ std<- as.numeric(strsplit(c," ")[[1]][1])
 so2mg <- cbind(so2mg,Months)
 
 attach(so2mg)
+
 #We can see the data of a paricular month 
 so2mg[Months=="Oct",]
 so2mg[Months=="Oct" & Concentration<80,] # do some analysis ,add one more column of levels : low, moderate , high ,according to Concentration
-
 
 #Adding a Column to show Exceedence Factor
 ExceedenceFactor<- so2mg$Concentration/std
@@ -213,27 +188,18 @@ so2mg<- cbind(so2mg,ExceedenceFactor)
 PollutionType<-cut(ExceedenceFactor,breaks=c(0,0.5,1.0,1.5,10),labels=c("Low","Moderate","High","Critical"),right = F)
 so2mg<- cbind(so2mg,PollutionType)
 attach(so2mg)
+
 #Now,Lets see the summary 
 summary(so2mg)
 
-
 #plotting
-#WHO Guideline = 50
 par(bg="gray91")
 
-#barplot(table(PollutionType),main = "Pollution Category",xlab ="Categories",ylab = "No. Of Days",las=1, names.arg = c(levels(PollutionType)),ylim = c(0,100),col ="damgorchid4",cex.axis = 0.8,cex.names =0.8)
-#box()
 table2<-table(PollutionType,Months)
 par(mar=c(3,5,3,3))
 barplot(table2,beside = T,main="Mandir Marg: Pollution Category For each Month for SO2",ylab="No.of Days",col=c("sienna1","salmon","orangered1","red3"),ylim =c(0,32))
 legend(11.67,30,legend=c("low","moderate","high","critical"),fill=c("sienna1","salmon","orangered1","red3"),bty="n",cex = 1)
 box()
-
-#par(adj=0.5)
-#pie(table(PollutionType),main = "Pollution Category")
-
-#boxplot(Concentration,main="Concentration of PM10",ylim=c(0,1000),las=1)
-#boxplot(Concentration~Months,main="Concentration of PM10 : mg Puram",ylim=c(0,1000),las=1,col=c("coral3","aliceblue","brown1"))
 
 par(mar=c(5,4,1,0))
 boxplot(Concentration~Months*PollutionType,main="Mandir Marg: Concentration of SO2",ylab="Concentration (µg/m3)",ylim=c(0,35),las=2,col=c("lightpink1","turquoise1","thistle"),cex.axis = 0.8)
@@ -241,16 +207,12 @@ boxplot(Concentration~Months*PollutionType,main="Mandir Marg: Concentration of S
 so2mg[Months=="Dec",]$Concentration
 summary(so2mg[Months=="Dec",]$Concentration)
 
-
-
+#Line Charts
 ggplot(so2mg,aes(so2mg$Date,so2mg$Concentration,color=2))+ geom_line(size = 1,show.legend = FALSE) + xlab("Months") + ylab("Concentration(µg/m3)") + ggtitle(" Mandir Marg : SO2 Concentration vs Months") + theme(axis.text.x = element_text(angle = 0, vjust = 0.5, color = "navy"))+geom_hline(yintercept = 80,color="red")+ ylim(0, 85)+geom_text(aes(as.Date("2016-11-10"),83),cex=3.7,color=2,fontface="italic",label="Prescribed Standard=80")+geom_hline(yintercept =mean(so2mg$Concentration),color="orchid3")+geom_text(aes(as.Date("2016-10-15"),15),cex=4,color="orchid4",fontface="italic",label="Mean=11.33")
 
 detach(so2mg)
 #rm(list = ls())
 
 
-
 ggplot(pm10mg,aes(Date,Concentration,color=5))+ geom_line(size = 1,color="5") + xlab("Dates") + ylab("Concentration(µg/m3)") + ggtitle(" Mandir Marg : Concentration of all Pollutants") + theme(axis.text.x = element_text(angle = 0, vjust = 0.5, color = "navy"))+ylim(0,950) +
   geom_line(data=so2mg,color=3)+geom_line(data=no2mg,color=4)+scale_colour_manual(values=c("5","3","4"))+theme_bw()+theme(legend.position="bottom")+geom_hline(yintercept = 80,color="red")+geom_hline(yintercept = 100,color="black")
-
-
